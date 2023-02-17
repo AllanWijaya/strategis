eb914d7dccee6a0a7bd85b3c101999ee7753cf0e
AllanWijaya <allangaulmen7@gmail.com>
2023-02-17 17:07:04 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Strategis | Market Place Property Terbaik</title>
+        <link rel="shortcut icon" href="logo/logo.png">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <body>
+        <nav class="navbar navbar-default bg-primary">
+            <div class="container-fluid bg-primary m-2">
+                <div class="navbar-header">
+                    <img src="logo/logo.png" href="#" alt="">
+                </div>
+                <div class="nav-item">
+                <div class="btn-group me-2">
+                <button class="btn dropdown-toggle" style="color:white;" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Sewa
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Rumah Disewa</a></li>
+                    <li><a class="dropdown-item" href="#">Ruko Disewa</a></li>
+                    <li><a class="dropdown-item" href="#">Tanah Disewa</a></li>
+                </ul>
+                </div>
+                <div class="btn-group">
+                <button class="btn dropdown-toggle" style="color:white;" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Jual
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Rumah Dijual</a></li>
+                    <li><a class="dropdown-item" href="#">Ruko Dijual</a></li>
+                    <li><a class="dropdown-item" href="#">Tanah Dijual</a></li>
+                </ul>
+                </div>
+                <div class="btn-group">
+                <button class="btn dropdown-toggle" style="color:white;" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Projek
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Rumah Dijual</a></li>
+                    <li><a class="dropdown-item" href="#">Ruko Dijual</a></li>
+                    <li><a class="dropdown-item" href="#">Tanah Dijual</a></li>
+                </ul>
+                <div class="btn-group">
+                    <a class="btn" style="color:white;" href="#">Blog</a>
+                </div>
+                <div class="btn-group" >
+                    <a class="btn" style="color:white;" href="#">About Us</a>
+                </div>
+                </div>
+                </div>
+                <div class="nav-item input-group-sm">
+                    <input type="search" class="form-control pe-3" maxlength="15" placeholder="Cari properti ">
+                </div>
+                <div class="nav-item">
+                    <a class="btn bg-white" href="http://wa.me/087772866945" target="_blank"><img src="logo/telephone-fill.svg" alt="Kontak kami"></a>
+                </div>
+            </div>
+        </nav>
+        
+    </body>
+</html>
